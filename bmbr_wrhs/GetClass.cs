2b07481b7c7a20a31686ec6861d7af81e8d122f3
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-07 13:12:04 +0700

    Create method getPartsNoNull

--- bmbr_wrhs/GetClass.cs
+++ bmbr_wrhs/GetClass.cs
@@ -16,5 +16,18 @@ namespace bmbr_wrhs
             db.Dispose();
             return data;
         }
+        public static List<AutoPart> getPartsNotNull()
+        {
+            AppContext db = new AppContext();
+            var data = db.Autoparts
+                .Where(u => u.Count !=0)
+                .Include(u => u.PartType)
+                .Include(u => u.Car)
+                .Include(u => u.Color)
+                .ThenInclude(c => c!.Color)
+                .ToList();
+            db.Dispose();
+            return data;
+        }
     }
 }
