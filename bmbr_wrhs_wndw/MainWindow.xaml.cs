3eb8fd2941d35b8cc8b73fc10c0103cbe9265d35
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-07 13:12:10 +0700

    Create Project Bumber Window in WPF Add two buttons, to load all and load not null parst

---
+++ bmbr_wrhs_wndw/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using bmbr_wrhs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bmbr_wrhs_wndw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void load_all_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.bumber_data_grid.ItemsSource = null;
+            this.bumber_data_grid.ItemsSource = GetClass.getAllAutopartsFromDB();
+        }
+
+        private void load_not_null_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.bumber_data_grid.ItemsSource = null;
+            this.bumber_data_grid.ItemsSource = GetClass.getPartsNotNull();
+        }
+    }
+}
