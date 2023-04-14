6b908cb7b89421673ef32bab0eb7b008bb3439a3
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-14 13:58:22 +0700

    Create SearchWindow in WPF

---
+++ bmbr_wrhs_wndw/SearchWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using bmbr_wrhs;
+
+namespace bmbr_wrhs_wndw
+{
+    /// <summary>
+    /// Interaction logic for SearchWindow.xaml
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        public SearchWindow()
+        {
+            InitializeComponent();
+            var data = GetClass.getAllCars();
+            autoBox.ItemsSource = data;
+        }
+
+        private void autoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            colorBox.ItemsSource = null;
+            var data = GetClass.getCarColor((int)autoBox.SelectedValue); 
+            colorBox.ItemsSource = data;
+            
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int carId = (int)autoBox.SelectedValue;
+                int colorId = (int)colorBox.SelectedValue;
+                if (carId > 0 && colorId > 0)
+                {
+                    MainWindow main = this.Owner as MainWindow;
+                    main.searchCarId = carId;
+                    main.searchColorId = colorId;
+                }                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Вы не выбрали");
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+    }
+}
