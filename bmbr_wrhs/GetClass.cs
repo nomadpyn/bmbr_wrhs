cb373462e203e64e475cf6b1839f38431c81ba8d
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-14 13:57:49 +0700

    Create methods -getAllCars -getCarColor -geAutoPartsbySearch

--- bmbr_wrhs/GetClass.cs
+++ bmbr_wrhs/GetClass.cs
@@ -29,5 +29,37 @@ namespace bmbr_wrhs
             db.Dispose();
             return data;
         }
+        public static List<Car> getAllCars()
+        {
+            AppContext db = new AppContext();
+            var data = db.Car.ToList();
+            db.Dispose();
+            return data;
+        }
+
+        public static List<CarColor> getCarColor(int id)
+        {
+            AppContext db = new AppContext();
+            var data = db.CarColor
+                .Where(c => c.CarBelong.Id == id)
+                .Include(c =>c.Color)
+                .ToList();
+            db.Dispose();
+            return data;
+        }
+        public static List<AutoPart> getAutoPartsbySearch(int carId, int ColorId)
+        {
+            AppContext db = new AppContext();
+            var data = db.Autoparts
+                .Where(c => c.Car.Id == carId)
+                .Where(c => c.Color.Id == ColorId)
+                .Include(u => u.PartType)
+                .Include(u => u.Car)
+                .Include(u => u.Color)
+                .ThenInclude(c => c!.Color)
+                .ToList();
+            db.Dispose();
+            return data;
+        }
     }
 }
