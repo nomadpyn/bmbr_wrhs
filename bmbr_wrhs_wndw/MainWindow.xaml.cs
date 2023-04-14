8cd8c249ad62307ce6d175eb51b47fe4d8a25a9e
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-14 13:58:34 +0700

    Add Search in MainWindow

--- bmbr_wrhs_wndw/MainWindow.xaml.cs
+++ bmbr_wrhs_wndw/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace bmbr_wrhs_wndw
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int searchCarId { get; set; }
+        public int searchColorId { get;set; }
         public MainWindow()
         {
             InitializeComponent();
@@ -38,5 +40,22 @@ namespace bmbr_wrhs_wndw
             this.bumber_data_grid.ItemsSource = null;
             this.bumber_data_grid.ItemsSource = GetClass.getPartsNotNull();
         }
+
+        private void search_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.searchCarId = 0;
+            this.searchColorId = 0;
+            this.bumber_data_grid.ItemsSource = null;
+            SearchWindow nw = new();
+            nw.Owner = this;
+            nw.ShowDialog();
+
+            if(this.searchCarId>0 && this.searchColorId > 0)
+            {
+                var data = GetClass.getAutoPartsbySearch(this.searchCarId, this.searchColorId);
+                this.bumber_data_grid.ItemsSource = data;
+            }
+        }
+        
     }
 }
