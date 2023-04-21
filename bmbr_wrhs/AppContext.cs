b475a9db97603b1cb66d96f05ffaa6b759faae45
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-04-21 13:45:20 +0700

    Add DBSet Color, PartType and SoldParts

--- bmbr_wrhs/AppContext.cs
+++ bmbr_wrhs/AppContext.cs
@@ -13,6 +13,9 @@ namespace bmbr_wrhs
         public DbSet<AutoPart> Autoparts { get; set; } = null!;
         public DbSet<Car> Car { get; set; } = null!;
         public DbSet<CarColor> CarColor { get; set; } = null!;
+        public DbSet<Color> Color { get; set; } = null!;
+        public DbSet<PartType> PartType { get; set; } = null!;
+        public DbSet<SoldPart> SoldParts { get; set; } = null!;
 
         public AppContext()
         {
