c067b25750beca1fbd09026569978ba5d560312d
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-05-03 10:25:37 +0700

    Update Program.cs in bmbr_wrhs

--- bmbr_wrhs/Program.cs
+++ bmbr_wrhs/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello Bumpers");
+﻿using bmbr_wrhs;
+using AppContext = bmbr_wrhs.AppContext;
+
+using (AppContext db = new())
+{
+    var data = GetClass.getAllAutopartsFromDB();
+    if(data != null)
+    {
+        Console.WriteLine("Подключение к БД успешно");
+    }
+}
