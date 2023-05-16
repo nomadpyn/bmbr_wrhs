6dedcf62e9264402bda599923a1c756ed03b8c61
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-05-16 11:04:46 +0700

    Add Program.cs to TelegramBot

---
+++ bmbr_wrhs_bot/Program.cs
@@ -0,0 +1,30 @@
+﻿using bmbr_wrhs_bot;
+using System.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+string Token = ConfigurationManager.ConnectionStrings["botToken"].ConnectionString;
+
+TelegramBotClient botClient = new TelegramBotClient(Token);
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: Services.HandleUpdateAsync,
+    pollingErrorHandler: Services.HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Начало работы бота @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
