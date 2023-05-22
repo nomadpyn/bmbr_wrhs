4f0c50cf7aaff8a38720a0f3d3317fa5d5f7f17e
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-05-22 17:59:07 +0700

    create txtLogger for TelegramBot

---
+++ bmbr_wrhs_bot/txtLogger.cs
@@ -0,0 +1,34 @@
+﻿
+namespace bmbr_wrhs_bot
+{
+    public class txtLogger
+    {
+        private readonly string path;
+        private readonly string dateString;
+
+        public txtLogger() 
+        {
+            this.path = createDirectory();
+            this.dateString = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();            
+        }
+
+        public async void log(string message)
+        {
+            string fullPath = path + "\\log" + this.dateString + ".txt";
+            using(StreamWriter writer = new(fullPath, true))
+            {
+                await writer.WriteLineAsync(DateTime.Now.TimeOfDay.ToString() + "   " + message);
+            }
+        }
+
+        private string createDirectory()
+        {
+            string subdir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+            if (!Directory.Exists(subdir))
+            {
+                Directory.CreateDirectory(subdir);
+            }
+            return subdir;
+        }
+    }
+}
